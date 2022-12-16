0316f35f674ba8214c06913147a02e6741c791b9
patelaftab <10patelaftab@gmail.com>
2022-12-16 15:45:36 +0530

    welcome message

---
+++ TestingGit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            int a=2; int b=3;
+            int Sum=0;
+            Sum = a+b;
+            Console.WriteLine("Adding Of Two Number Is :" + Sum);
+            Console.ReadLine();
+        }
+    }
+}
